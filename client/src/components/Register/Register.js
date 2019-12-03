bb35b92d091b44fd788c9761862e5172d31aa59b
deondrejemison <54921154+deondrejemison@users.noreply.github.com>
2019-12-02 19:52:33 -0600

    Problem with Update Render

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const Register = () => {
     const [userData, setUserData] = useState({
@@ -46,6 +47,8 @@ const Register = () => {
             }
         }
     }
+
+
 
         return (
         <div>
